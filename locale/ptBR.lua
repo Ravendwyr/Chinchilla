5c57722cb97d70939e0eacdbadbbc8839502330a
Ethan Centaurai <stevenblanchard86@gmail.com>
2013-03-10 19:34:49 +0000

    Add dummy file for Portugese translations.

---
+++ locale/ptBR.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Chinchilla", "frFR")
+if not L then return end
+
+--[===[@non-debug@
+@localization(locale="frFR", format="lua_additive_table")@
+--@end-non-debug@]===]
