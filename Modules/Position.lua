75bdcdb26bc6dbf15e9eeeb8f8c4823ad3638400
Ethan Centaurai <ethancentaurai@outlook.com>
2013-09-02 14:42:22 +0100

    Position: Forcibly disable the module if MoveAnything is installed.

--- Modules/Position.lua
+++ Modules/Position.lua
@@ -1,3 +1,5 @@
+if IsAddOnLoaded("MoveAnything") then return end
+
 
 local Position = Chinchilla:NewModule("Position", "AceEvent-3.0", "AceHook-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Chinchilla")
